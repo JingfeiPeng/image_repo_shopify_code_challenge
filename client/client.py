cd50f008a5bf47c3b4145162920f01dc5418e5ac
Jingfei Peng <36685292+JingfeiPeng@users.noreply.github.com>
2021-05-05 00:38:06 -0400

    added options in client

--- client/client.py
+++ client/client.py
@@ -2,6 +2,7 @@ from os import listdir
 from os.path import isfile, join
 import aiohttp
 import asyncio
+import argparse
 import requests
 import os
 
@@ -20,17 +21,42 @@ async def post_file(image_path):
     return data
 
 
-async def main():
+async def main(path, is_file=False) -> None:
     async with aiohttp.ClientSession() as session:
         tasks = []
-        for f in listdir(
-            join(os.path.dirname(os.path.realpath(__file__)), "sample_images")
-        ):
-            file_path = join(os.path.dirname(os.path.realpath(__file__)), "sample_images", f)
-            if isfile(file_path):
-                tasks.append(post_file(file_path))
+        if not is_file:
+            # sends all files at path directory
+            for f in listdir(path):
+                file_path = join(path, f)
+                if isfile(file_path):
+                    tasks.append(post_file(file_path))
+        elif isfile(path):
+            # sends a single file
+            tasks.append(post_file(path))
+        elif isfile(join(os.path.dirname(os.path.realpath(__file__)),path)):
+            # allow relative path for a single file
+            tasks.append(post_file(join(os.path.dirname(os.path.realpath(__file__)),path)))
+        else:
+            print(f"ERROR: {path} is not a valid file path")
         results = await asyncio.gather(*tasks)
         print(results)
 
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    parser = argparse.ArgumentParser(
+        prog="image repo client",
+        description="a script to upload images based on a path, can upload individual files or every file in a folder",
+    )
+    parser.add_argument(
+        "--path",
+        help="path to the folder of images to send, or a individual file, ",
+        default=join(os.path.dirname(os.path.realpath(__file__)), "sample_images"),
+        type=str,
+    )
+    parser.add_argument(
+        "--is_file",
+        help="whether the fed in path is pointing to a file, by default assumes the path points to a folder",
+        default=False, action="store_true"
+    )
+    args = parser.parse_args()
+    asyncio.run(main(args.path, args.is_file))
